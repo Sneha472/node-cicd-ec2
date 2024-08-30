17ff738a2d3ffe026582bdf6e434ae8f6f2d9731
Sneha <snehaadhi94@gmail.com>
2024-08-30 15:51:59 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
   try {
+    console.log(process.env.MONGO_URI);
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
